019199a4e9b31b26af6edf1960a62f4720841fb2
Mannyg448 <manny.g448@hotmail.com>
2022-04-06 01:38:18 -0500

    Added Express.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import express from 'express'
+
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send({ msg: 'Hello There' })
+})
+
+app.listen(port)
