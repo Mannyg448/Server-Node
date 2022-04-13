792a5f74e5d1c9c8f4a0814a41463c7bbdaf0d12
Mannyg448 <manny.g448@hotmail.com>
2022-04-12 23:02:37 -0500

    creating id and validation for the api

--- src/routes/api/todos.js
+++ src/routes/api/todos.js
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { Router } from 'express'
-import {getTodos} from '../../utils/todos'
+import { StatusCodes } from 'http-status-codes'
+import {addTodo, getTodos, getTodo } from '../../utils/todos'
 
 const router = Router()
 
@@ -8,6 +9,29 @@ router.get('/', (req, res) => {
     const todos = getTodos()
     res.send(todos)
 
+})
+
+router.get('/:id', (req, res, next) => {
+    const todo = getTodo(req.params.id)
+    if (todo) {
+        res.send(todo)
+    } else {
+        res.status(StatusCodes.NOT_FOUND)
+        next('Not Found')
+    }
+})
+
+
+router.post('/', (req, res, next) => {
+    const todo = req.body
+    const  response  = addTodo(todo)
+    if (response.error) {
+        res.status(StatusCodes.BAD_REQUEST)
+        next(response.error)
+    } else {
+        res.status(StatusCodes.CREATED)
+    res.send(response.newTodo)
+    }
 })
 
 export default router
--- src/utils/todos.js
+++ src/utils/todos.js
@@ -1,7 +1,15 @@
+/* eslint-disable consistent-return */
 /* eslint-disable prettier/prettier */
 import { nanoid } from 'nanoid'
+import Joi from 'joi'
+import logger from './logger'
 
 const todos = []
+
+const todoSchema = Joi.object({
+    text: Joi.string().min(10).required(),
+    completed: Joi.boolean().required(),
+})
 
 const baseTodo = {
     id: nanoid(),
@@ -12,10 +20,25 @@ const baseTodo = {
 todos.push(baseTodo)
 
 export const getTodos = () => {
+    logger.log.success('Getting todos')
     return todos
+}
+export const getTodo = (id) => {
+    logger.log.success(`Getting todo with id: ${id}`)
+    return todos.find((todo) => todo.id === id)
 }
 
 export const addTodo = (todo) => {
-    todos.push(todo)
+    logger.log.info(`Validating ${todo}`)
+    const { error } = todoSchema.validate(todo)
+
+    if (error) {
+        logger.log.error(new Error(`Validation error: ${error.message}`))
+        return { error }
+    }
+    logger.log.success(`Validated: ${todo}`)
+    const newTodo = { id: nanoid(), ...todo}
+    todos.push({ id:nanoid(), ...todo })
+    return { newTodo }
 
 }
